04efd2050c2ae9cfa21c876754c038665c67afd3
Nityaa1802 <nitya.agarwal_cs.aiml20@gla.ac.in>
2023-04-17 19:11:47 +0530

    implemented payment by stripe and secure localhost

--- src/lib/oktaConfig.ts
+++ src/lib/oktaConfig.ts
@@ -2,7 +2,7 @@ import { redirect } from "react-router-dom";
 export const oktaConfig = {
     clientId: '0oa8q67wgbDGQVWTr5d7',
     issuer: 'https://dev-50692489.okta.com/oauth2/default',
-    redirectUri: 'http://localhost:3000/login/callback',
+    redirectUri: 'https://localhost:3000/login/callback',
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
     disableHttpsCheck: true,
