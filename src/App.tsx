9c3e7ace9788af050f1d42328ad1382ff9a60dd8
Nityaa1802 <nitya.agarwal_cs.aiml20@gla.ac.in>
2023-04-17 19:13:17 +0530

    Ended frontend

--- src/App.tsx
+++ src/App.tsx
@@ -8,17 +8,26 @@ import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
 import { SearchBooksPage } from "./layouts/SearchBooksPage/SearchBooksPage";
 import { oktaConfig } from "./lib/oktaConfig";
 import { OktaAuth, toRelativeUrl } from '@okta/okta-auth-js'
-import {LoginCallback, Security} from '@okta/okta-react'
+import { Security, LoginCallback, SecureRoute, useOktaAuth } from '@okta/okta-react';
 import LoginWidget from "./Auth/LoginWidget";
+import { ReviewListPage } from "./layouts/BookCheckoutPage/ReviewListPage/RewiewListPage";
+import ShelfPage from "./layouts/ShelfPage/ShelfPage";
+import { MessagesPage } from "./layouts/MessagesPage/MessagesPage";
+import { ManageLibraryPage } from "./layouts/ManageLibraryPage/ManageLibrary";
+import { PaymentPage } from "./layouts/PaymentPage/PaymentPage";
+
 
 
 const oktaAuth = new OktaAuth(oktaConfig)
+
 function App() {
+
 
 
   const customAuthHandler = () => {
     navigate('/login');
   }
+  //  const { authState } = useOktaAuth();
   const navigate = useNavigate();
   const restoreOriginalUri = async (_oktaAuth: any, originalUri: any) => {
     navigate(toRelativeUrl(originalUri||'/',window.location.origin),{replace:true})
@@ -28,7 +37,7 @@ function App() {
 
   return (
     <div className="d-flex flex-column min-vh-100">
-      <Security oktaAuth={oktaAuth} restoreOriginalUri={restoreOriginalUri} onAuthRequired={customAuthHandler}>
+     <Security oktaAuth={oktaAuth} restoreOriginalUri={restoreOriginalUri} onAuthRequired={customAuthHandler}>
       <Navbar />
       <div className="flex-grow-1">
           <Routes>
@@ -36,12 +45,19 @@ function App() {
             
           <Route path="/home" element={<Homepage />} />
           <Route path="/" element={<Homepage />} />
-          <Route path="/search" element={<SearchBooksPage />} />
+            <Route path="/search" element={<SearchBooksPage />} />
+            <Route path="/reviewList/:bookId" element={<ReviewListPage/>} />
             <Route path="/checkout/:bookId" element={<BookCheckoutPage />} />
             <Route path="/login" element={
+
               <LoginWidget config={oktaConfig}/>
             } />
-            <Route path="/login/callback" Component={LoginCallback}/>
+            <Route path="/login/callback" Component={LoginCallback} />
+            <Route path="/shelf" Component={ShelfPage}/>
+            <Route path="/message" Component={MessagesPage} />
+            <Route path="/admin" Component={ManageLibraryPage} />
+            <Route path="/fees" Component={PaymentPage}/>
+         
         </Routes>
       </div>
       <Footer />
--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,19 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
 
+const stripePromise = loadStripe(
+  "pk_test_51Mwl5xSJ8zehHEVfGUPkLYNo8arXXio2nnnVkLpD2K1y4vIzX61A1u2a0hiEuFhJFYbRJkIzqY3XqAlUgkS2LlVC00tVIs0Uf5"
+);
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
+  <BrowserRouter>
+    <Elements stripe={stripePromise}>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    </Elements>
+  </BrowserRouter>
 );
